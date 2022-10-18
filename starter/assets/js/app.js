f316c84e79cf7304ee6917f4ce368467f03f6b8d
mohamed chkir <mohamedchkir2@gmail.com>
2022-10-18 17:31:59 +0100

    commencer la fonction d'ajouter

---
+++ starter/assets/js/app.js
@@ -0,0 +1,146 @@
+
+
+
+// check if there is data in local storage and effected to the array
+if (localStorage.getItem("tasks")) {
+    allTasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+// call function
+getDataFromLocalStorage();
+
+// print all tasks from local storage
+function getDataFromLocalStorage() {
+    let data = window.localStorage.getItem("tasks");
+    if (data) {
+        let tasks = JSON.parse(data);
+        printTasks(tasks);
+    }
+}
+
+// add array of tasks to local storage
+function addDataToLocalStorageFrom(tasks) {
+    window.localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function printTasks(AllTasks) {
+    // truncate all tasks
+    document.querySelector("#toDo").innerHTML = "";
+    document.querySelector("#inProgress").innerHTML = "";
+    document.querySelector("#done").innerHTML = "";
+    let toDo = document.querySelector("#toDo");
+    let inProgress = document.querySelector("#inProgress");
+    let done = document.querySelector("#done");
+
+    AllTasks.forEach((task) => {
+        // AFFICHER TASKS
+        if (task.status === "To do") {
+            toDo.innerHTML +=
+                ` <button class="bg-white w-100 d-flex border-0 pb-2">
+                  <div class="px-2 text-start mt-3">
+                    <i class="text-start bi bi-question-circle text-success fa-lg mt-4"></i>
+                  </div>
+                  <div class="text-start mt-2">
+                    <div class="h6">${task.title}</div>
+                    <div class="text-start">
+                      <div>#1 created ${task.date}</div>
+                      <div
+                        title="" >
+                      ${task.description}
+                      </div>
+                    </div>
+                    <div class="">
+                      <span class="btn btn-primary" style="font-size: 0.6rem; padding: 2px 6px">${task.priority}</span>
+                      <span class="btn btn-gray-400 text-dark" style="font-size: 0.6rem; padding: 2px 6px">${task.type}</span>
+                    </div>
+                  </div>
+                </button>`;
+
+        } else if (task.status === "In progress") {
+            inProgress.innerHTML +=
+                `<button class="bg-white w-100 d-flex border-0 pb-2">
+                <div class="px-2 text-start mt-3">
+                    <i class="text-start fa fa-circle-notch text-success fa-lg"></i>
+                  </div>
+                <div class="text-start mt-2">
+                  <div class="h6">${task.title}</div>
+                  <div class="text-start">
+                    <div>#1 created ${task.date}</div>
+                    <div
+                      title="" >
+                    ${task.description}
+                    </div>
+                  </div>
+                  <div class="">
+                    <span class="btn btn-primary" style="font-size: 0.6rem; padding: 2px 6px">${task.priority}</span>
+                    <span class="btn btn-gray-400 text-dark" style="font-size: 0.6rem; padding: 2px 6px">${task.type}</span>
+                  </div>
+                </div>
+              </button>`;
+        } else if (task.status === "Done") {
+            done.innerHTML +=
+                `<button class="bg-white w-100 d-flex border-0 pb-2">
+                <div class="px-2 mt-3 text-start">
+                <i class="text-start far fa-check-circle text-success fa-lg"></i>
+              </div>
+                <div class="text-start mt-2">
+                  <div class="h6">${task.title}</div>
+                  <div class="text-start">
+                    <div>#1 created ${task.date}</div>
+                    <div
+                      title="" >
+                    ${task.description}
+                    </div>
+                  </div>
+                  <div class="">
+                    <span class="btn btn-primary" style="font-size: 0.6rem; padding: 2px 6px">${task.priority}</span>
+                    <span class="btn btn-gray-400 text-dark" style="font-size: 0.6rem; padding: 2px 6px">${task.type}</span>
+                  </div>
+                </div>
+              </button>`;
+        }
+
+    });
+}
+
+
+
+var form = document.getElementById("form");
+
+form.addEventListener('submit', (M) => {
+
+    // to stop reload fo page
+    M.preventDefault();
+
+    // get all info from form
+
+    var title = document.getElementById("title").value;
+    var type = document.querySelector('input[name="type"]:checked');
+    var priority = document.getElementById('priority').value;
+    var option = document.getElementById('status').value;
+    var date = document.getElementById("date").value;
+    var description = document.getElementById("description").value;
+
+    // truncate form
+    document.getElementById("title").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("description").value = "";
+
+    // create object of task
+    var task = {
+        title: title,
+        type: type,
+        priority: priority,
+        status: option,
+        date: date,
+        description: description
+    };
+
+    // add task to array of tasks
+    allTasks.push(task);
+    // print all tasks
+    printTasks(allTasks);
+    // storage tasks in local storage
+    addDataToLocalStorageFrom(allTasks)
+
+})
